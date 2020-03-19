30ba2493822944cd24dce134cdcef44d974dea39
shenwenjie <wenjie.shen@zd-automotive.de>
2020-03-19 04:39:45 +0100

    test

--- zd-appium-server/src/frontend/res/static/js/main.1adb232c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),p=n(2);var c=function(){return r.a.createElement("div",{className:"App"},"hello react")};n.n(p).a.render(r.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
-//# sourceMappingURL=main.1adb232c.chunk.js.map
